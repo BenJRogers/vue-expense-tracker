3152ae5d25ebd94debeb0caaff0cdfb3d608a503
Benjamin Rogers <benjrogers48@gmail.com>
2024-03-22 23:49:15 +1100

    Learning new Composition API

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,25 @@
     <div class="container">
         <Balance />
         <IncomeExpenses />
-        <TransactionList />
+        <TransactionList :transactions="transactions" />
         <AddTransaction />
     </div>
 
 </template>
 
-<script>
+<script setup>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 
-export default {
-    components: {
-        Header,
-        Balance,
-        IncomeExpenses,
-        TransactionList,
-        AddTransaction
-    },
-};
+import { ref } from 'vue';
+
+const transactions = ref([
+    { id: 1, text: 'Flower', amount: -10.99 },
+    { id: 2, text: 'Bike', amount: 50.99 },
+    { id: 3, text: 'Laptop', amount: -2000 },
+    { id: 4, text: 'Web Work', amount: 5000 }
+]);
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,6 +1,10 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
+        <li v-for="transaction in transactions" :key="transaction.id"
+            :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>{{ transaction.amount }}</span><button class="delete-btn">x</button>
+        </li>
         <!-- <li class="minus">
             Cash <span>-$400</span><button class="delete-btn">x</button>
         </li>
@@ -10,17 +14,55 @@
     </ul>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            transactions: [
-                { id: 1, text: 'Flower', amount: -10.99 },
-                { id: 2, text: 'Bike', amount: -50.99 },
-                { id: 3, text: 'Laptop', amount: -2000 },
-                { id: 4, text: 'Web Work', amount: 5000 }
-            ]
-        }
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true
     }
-}
+});
+
+
 </script>
+
+//Composition API Shorthand
+<!-- <script setup>
+const transactions = [
+    { id: 1, text: 'Flower', amount: -10.99 },
+    { id: 2, text: 'Bike', amount: 50.99 },
+    { id: 3, text: 'Laptop', amount: -2000 },
+    { id: 4, text: 'Web Work', amount: 5000 }
+];
+</script> -->
+
+<!-- <script>
+export default {
+
+    // Composition API
+    setup() {
+        const transactions = [
+            { id: 1, text: 'Flower', amount: -10.99 },
+            { id: 2, text: 'Bike', amount: 50.99 },
+            { id: 3, text: 'Laptop', amount: -2000 },
+            { id: 4, text: 'Web Work', amount: 5000 }
+        ];
+        return {
+            transactions
+        }
+    } -->
+
+//Options API
+// data() {
+// return {
+// transactions: [
+// { id: 1, text: 'Flower', amount: -10.99 },
+// { id: 2, text: 'Bike', amount: 50.99 },
+// { id: 3, text: 'Laptop', amount: -2000 },
+// { id: 4, text: 'Web Work', amount: 5000 }
+// ]
+// }
+// },
+}
+<!-- </script> -->
