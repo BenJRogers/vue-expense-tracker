8e66d9b262b1ff7690c7227d07f0a204add91357
Benjamin Rogers <benjrogers48@gmail.com>
2024-03-23 23:49:20 +1100

    added income and expense totals

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,10 @@
     <Header />
     <div class="container">
         <Balance :totalAmount="totalAmount" />
-        <IncomeExpenses />
+        <IncomeExpenses :income="income" :expenses="expenses" />
         <TransactionList :transactions="transactions" />
         <AddTransaction />
     </div>
-
 </template>
 
 <script setup>
@@ -15,6 +14,7 @@ import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
+
 
 import { ref, computed } from 'vue';
 
@@ -29,5 +29,18 @@ const totalAmount = computed(() => {
     return transactions.value.reduce((acc, transaction) => {
         return acc + transaction.amount
     }, 0)
-})
+});
+
+const income = computed(() => {
+    return transactions.value.filter((transaction) => transaction.amount > 0).reduce((acc, transaction) => {
+        return acc + transaction.amount
+    }, 0).toFixed(2);
+});
+
+const expenses = computed(() => {
+    return transactions.value.filter((transaction) => transaction.amount < 0).reduce((acc, transaction) => {
+        return acc + transaction.amount
+    }, 0).toFixed(2);
+});
+
 </script>
